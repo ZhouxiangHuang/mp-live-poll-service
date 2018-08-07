424bbb17b96d6fa5d27741fb79bb5af7ac0a5a9d
Uncle Joe <zhouxiang.huang@gmail.com>
2018-08-07 19:47:09 +0800

    koa added

--- server.js
+++ server.js
@@ -1,16 +1,9 @@
-// const Koa = require('koa');
-// const app = new Koa();
+const Koa = require('koa');
+const app = new Koa();
 
-// // response
-// app.use(ctx => {
-//   ctx.body = 'Hello Koa';
-// });
+const main = ctx => {
+  ctx.response.body = 'Hello World!';
+};
 
-// app.listen(3000);
-
-
-for (let i = 0; i < 3; i++) {
-  setTimeout(function() {
-    console.log(i);
-  }, 0);
-}
+app.use(main);
+app.listen(3000);

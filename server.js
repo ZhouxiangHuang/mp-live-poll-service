3dded76b8184955c339b14c99ce8260bce13432b
Uncle Joe <zhouxiang.huang@gmail.com>
2018-08-07 21:41:21 +0800

    socket

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 const Koa = require('koa');
 const app = new Koa();
+const fs = require('fs');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 
 const main = ctx => {
-  ctx.response.body = 'Hello World!';
+  ctx.type = 'html';
+  ctx.body = fs.createReadStream('./index.html');
 };
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
 
 app.use(main);
 app.listen(3000);

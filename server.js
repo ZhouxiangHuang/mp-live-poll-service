8eac788ae2fc032bfae6cc2e2ab4b30916f805f6
unknown <1102424705@qq.com>
2018-08-07 23:38:45 +0800

    类似聊天室

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const Koa = require('koa');
 const app = new Koa();
 const fs = require('fs');
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// var http = require('http').Server(app);
+// var io = require('socket.io')(http);
+const IO = require('koa-socket');
+const io = new IO();
+io.attach(app)
 
 
 const main = ctx => {
@@ -10,9 +13,22 @@ const main = ctx => {
   ctx.body = fs.createReadStream('./index.html');
 };
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
+app._io.on('connection', socket => {
+  console.log('建立连接了');
+  
+  socket.on('serverListen', function (clientMsg) {
+    console.log(clientMsg);
+  });
+
+  socket.emit('serverEmit', '我接收到增加购物车的事件了');  /*发给指定用户*/
+
+  app._io.emit('serverEmit', '我接收到增加购物车的事件了');  /*广播*/
+
+})
 
 app.use(main);
 app.listen(3000);

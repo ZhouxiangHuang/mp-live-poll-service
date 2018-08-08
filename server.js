4f7c9e951b052b4db3d181442372a0b8d6aab961
Uncle Joe <zhouxiang.huang@gmail.com>
2018-08-08 12:12:55 +0800

    chat room demo

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const Koa = require('koa');
 const app = new Koa();
 const fs = require('fs');
-// var http = require('http').Server(app);
-// var io = require('socket.io')(http);
 const IO = require('koa-socket');
 const io = new IO();
 io.attach(app)
@@ -12,22 +10,21 @@ const main = ctx => {
   ctx.body = fs.createReadStream('./index.html');
 };
 
-// io.on('connection', function(socket){
-//   console.log('a user connected');
-// });
-
 app._io.on('connection', socket => {
   console.log('建立连接了');
-  
-  socket.on('serverListen', function (clientMsg) {
-    console.log(clientMsg);
+  let clientId = socket.client.id;
+
+  socket.on('chat message', function (msg) {
+    app._io.emit('chat message', msg);
+    console.log('client id: ' + clientId + 'message: ' + msg);
   });
 
-  socket.emit('serverEmit', '我接收到增加购物车的事件了');  /*发给指定用户*/
+  // socket.emit('serverEmit', '我接收到增加购物车的事件了'); /*发给指定用户*/
 
-  app._io.emit('serverEmit', '我接收到增加购物车的事件了');  /*广播*/
+  // app._io.emit('serverEmit', '我接收到增加购物车的事件了'); /*广播*/
 
 })
+
 
 app.use(main);
 app.listen(3000);

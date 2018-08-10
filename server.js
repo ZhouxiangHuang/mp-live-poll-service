328cc00affa034e8e2e05d6a229be5c48fab23e6
Uncle Joe <zhouxiang.huang@gmail.com>
2018-08-10 13:37:15 +0800

    socket io module added

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const Koa = require('koa');
 const app = new Koa();
 const router = require('./router');
 const socket = require('./socket')(app);
+const fs = require('fs');
+
+
+const main = ctx => {
+  ctx.type = 'html';
+  ctx.body = fs.createReadStream('./index.html');
+};
 
 app
     .use(router.routes())
     .use(router.allowedMethods())
-
+    .use(main)
     .listen(3000);
--- socket.js
+++ socket.js
@@ -4,9 +4,12 @@ const io = new IO();
 const socket = app => {
     io.attach(app);
 
-    io.on('connect', (ctx, data) => {
+    app._io.on('connect', (ctx, data) => {
         console.log('connected!');
     })
+
+    app._io.emit('chat message', 'tsup');
+
 }
 
 module.exports = socket;
